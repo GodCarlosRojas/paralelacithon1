ad104e6f835e22866364f2ae6838499b9e3d9b37
GodCarlosRojas <109989522+GodCarlosRojas@users.noreply.github.com>
2022-11-03 16:59:13 -0500

    Delete setup.py

--- setup.py
+++
@@ -1,9 +0,0 @@
-# Fichero para la creacion del objeto compartido
-
-from distutils.core import setup, Extension
-from Cython.Build import cythonize
-
-exts = (cythonize("cy_planet.pyx"))
-setup(ext_modules = exts)
-
-
